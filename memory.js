0f52e0289fb8e8ed2221c4cea08191af3df21dd2
Moskwa Quentin moskwaquentin@gmail.com <95235691+QuentinMoskwa@users.noreply.github.com>
2025-06-16 15:29:23 +0200

    ajout de la logique de chargement du json et lock si vérification.

--- memory.js
+++ memory.js
@@ -2,20 +2,15 @@ const gridContainer = document.querySelector(".grid-container");
 let cards = [];
 let firstReturnedCard = null;
 let secondReturnedCard = null;
+let lock = false;
 
-// Card data -> name, image
-cards = [
-{ name: "repo1", image: "img/1.png" },
-{ name: "repo2", image: "img/2.png" },
-{ name: "repo3", image: "img/3.png" },
-{ name: "repo4", image: "img/4.png" },
-{ name: "repo5", image: "img/5.png" },
-{ name: "repo6", image: "img/6.png" },
-{ name: "repo7", image: "img/7.png" },
-{ name: "repo8", image: "img/8.png" },
-];
 
-cards = cards.concat(cards);
+fetch("./data/cards.json")
+  .then((res) => res.json())
+  .then((data) => {
+    cards = [...data, ...data];
+    launchGame();
+  });
 
 function shuffleCards() {
   let currentIndex = cards.length,
@@ -47,6 +42,7 @@ function generateCards() {
 }
 
 function flipCard() {
+    if (lock) return;
     if (this === firstReturnedCard) return;
 
     this.classList.add("flipped");
@@ -56,6 +52,8 @@ function flipCard() {
         return;
     }
     secondReturnedCard = this;
+
+    lock = true;
     checkForMatch();
 }
 
@@ -65,18 +63,22 @@ function checkForMatch() {
     if(isMatch) {
         firstReturnedCard.removeEventListener("click", flipCard);
         secondReturnedCard.removeEventListener("click", flipCard);
-        firstReturnedCard = null;
-        secondReturnedCard = null;
+        resetBoard();
     }
     else {
         setTimeout(() => {
         firstReturnedCard.classList.remove("flipped");
         secondReturnedCard.classList.remove("flipped");
-        firstReturnedCard = null;
-        secondReturnedCard = null;
+        resetBoard();
         }, 1000);
     }
 
+}
+
+function resetBoard() {
+    firstReturnedCard = null;
+    secondReturnedCard = null;
+    lock = false;
 }
 
 
